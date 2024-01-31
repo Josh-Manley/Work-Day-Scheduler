fa1fa3199ed93779665a0105d2b9dd5238d24b30
Josh-Manley <joshmanley45@gmail.com>
2024-01-30 18:44:30 -0700

    added function and if statements to update color of time blocks

--- Develop/script.js
+++ Develop/script.js
@@ -34,6 +34,23 @@ $(function () {
       container.append(timeBlock);
     }
 
+     // Color Code Time Blocks
+     function updateColorCode() {
+      const currentHour = dayjs().hour();
+
+      $(".time-block").each(function () {
+        const blockHour = parseInt($(this).find(".hour").text().replace(/[^\d]/g, ""));
+  
+        if (blockHour < currentHour) {
+          $(this).removeClass("present future").addClass("past");
+        } else if (blockHour === currentHour) {
+          $(this).removeClass("past future").addClass("present");
+        } else {
+          $(this).removeClass("past present").addClass("future");
+        }
+      });
+    }
+
  
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
